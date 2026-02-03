76273a2f44d1c7c6525abb725bfdfed865c3409b
phuccom000 <gdtitans6@gmail.com>
2026-02-03 13:13:34 +0700

    avoided calling isDamageable() in modifyDurabilityBasedOnQuality

--- src/main/java/net/stirdrem/overgeared/mixin/ItemStackMixin.java
+++ src/main/java/net/stirdrem/overgeared/mixin/ItemStackMixin.java
@@ -61,11 +61,12 @@ public abstract class ItemStackMixin {
     private void modifyDurabilityBasedOnQuality(CallbackInfoReturnable<Integer> cir) {
         ItemStack stack = (ItemStack) (Object) this;
 
-        if (!stack.isDamageableItem()) {
+        int originalDurability = cir.getReturnValue();
+
+        if (originalDurability <= 0) {
             return;
         }
 
-        int originalDurability = cir.getReturnValue();
         boolean blacklisted = ItemUtils.isDurabilityBlacklisted(stack);
 
         float baseMultiplier = ServerConfig.BASE_DURABILITY_MULTIPLIER.get().floatValue();
